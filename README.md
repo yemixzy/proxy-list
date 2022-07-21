05467b4b4a2a698f5a661f004710288d8dafdcb5
yemix <107434247+0x000d023f@users.noreply.github.com>
2022-07-21 21:20:10 +0200

    Upload Proxy Scraper!

---
+++ README.md
@@ -0,0 +1,38 @@
+
+# Free HTTP Proxy List 🌍
+
+Free Proxy List for everyone
+
+> Scraper found **3890** proxies at the latest update. Usable proxies are below.
+
+## Usage
+
+Click the file format that you want and copy the URL.
+
+
+|File|Content|Count|
+|----|-------|-----|
+|[data.txt](https://raw.githubusercontent.com/yemixzy/http-proxy/main/proxy-list/data.txt)|`ip_address:port` combined (seperated new line)|188|
+|[data.json](https://raw.githubusercontent.com/yemixzy/http-proxy/main/proxy-list/data.json)|`ip, port`|188|
+|[data-with-geolocation.json](https://raw.githubusercontent.com/yemixzy/http-proxy/main/proxy-list/data-with-geolocation.json)|`ip, port, geolocation`|188|
+
+## Sources
+
+|Source|Found Proxies|Succeed|
+|------|-------------|-------|
+|[free-proxy-list.net](https://free-proxy-list.net)|300|✅|
+|[us-proxy.org](https://www.us-proxy.org)|200|✅|
+|[proxydb.net](http://proxydb.net)|15|✅|
+|[free-proxy-list.com](https://free-proxy-list.com/?page=&port=&type%5B%5D=http&type%5B%5D=https&up_time=0&search=Search)|10|✅|
+|[proxy-list.download](https://www.proxy-list.download/HTTP)|26|✅|
+|[vpnoverview.com](https://vpnoverview.com/privacy/anonymous-browsing/free-proxy-servers)|32|✅|
+|[proxyscan.io](https://www.proxyscan.io)|100|✅|
+|[proxyscrape.com](https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all)|248|✅|
+|[github.com/clarketm/proxy-list](https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt)|400|✅|
+|[github.com/monosans/proxy-list](https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt)|221|✅|
+|[github.com/TheSpeedX/PROXY-List](https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt)|1801|✅|
+|[github.com/almroot/proxylist](https://raw.githubusercontent.com/almroot/proxylist/master/list.txt)|6|✅|
+|[github.com/ShiftyTR/Proxy-List](https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/http.txt)|19|✅|
+|[github.com/mmpx12/proxy-list](https://raw.githubusercontent.com/mmpx12/proxy-list/master/http.txt)|512|✅|
+
+
---
+++ sources.py
@@ -0,0 +1,149 @@
+SOURCES = [
+    {
+        "id": "free-proxy-list.net",
+        "url": "https://free-proxy-list.net",
+        "method": "GET",
+        "parser": {
+            "pandas": {
+                "table_index": 0,
+                "ip": "IP Address",
+                "port": "Port",
+                "combined": None,
+            },
+        }
+    },
+    {
+        "id": "us-proxy.org",
+        "url": "https://www.us-proxy.org",
+        "method": "GET",
+        "parser": {
+            "pandas": {
+                "table_index": 0,
+                "ip": "IP Address",
+                "port": "Port",
+                "combined": None,
+            },
+        }
+    },
+    {
+        "id": "proxydb.net",
+        "url": "http://proxydb.net",
+        "method": "GET",
+        "parser": {
+            "pandas": {
+                "table_index": 0,
+                "ip": None,
+                "port": None,
+                "combined": "Proxy",
+            },
+        }
+    },
+    {
+        "id": "free-proxy-list.com",
+        "url": "https://free-proxy-list.com/?page=&port=&type%5B%5D=http&type%5B%5D=https&up_time=0&search=Search",
+        "method": "GET",
+        "parser": {
+            "pandas": {
+                "table_index": 1,
+                "ip": "IP Address",
+                "port": "Port",
+                "combined": None,
+            },
+        }
+    },
+    {
+        "id": "proxy-list.download",
+        "url": "https://www.proxy-list.download/HTTP",
+        "method": "GET",
+        "parser": {
+            "pandas": {
+                "table_index": 0,
+                "ip": "IP Address",
+                "port": "Port",
+                "combined": None,
+            },
+        }
+    },
+    {
+        "id": "vpnoverview.com",
+        "url": "https://vpnoverview.com/privacy/anonymous-browsing/free-proxy-servers",
+        "method": "GET",
+        "parser": {
+            "pandas": {
+                "table_index": 0,
+                "ip": "IP address",
+                "port": "Port",
+                "combined": None,
+            },
+        }
+    },
+    {
+        "id": "proxyscan.io",
+        "url": "https://www.proxyscan.io",
+        "method": "GET",
+        "parser": {
+            "pandas": {
+                "table_index": 0,
+                "ip": "Ip Address",
+                "port": "Port",
+                "combined": None,
+            },
+        }
+    },
+    {
+        "id": "proxyscrape.com",
+        "url": "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all",
+        "method": "GET",
+        "parser": {
+            "txt": {},
+        }
+    },
+    {
+        "id": "github.com/clarketm/proxy-list",
+        "url": "https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt",
+        "method": "GET",
+        "parser": {
+            "txt": {},
+        }
+    },
+    {
+        "id": "github.com/monosans/proxy-list",
+        "url": "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt",
+        "method": "GET",
+        "parser": {
+            "txt": {},
+        }
+    },
+    {
+        "id": "github.com/TheSpeedX/PROXY-List",
+        "url": "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt",
+        "method": "GET",
+        "parser": {
+            "txt": {},
+        }
+    },
+    {
+        "id": "github.com/almroot/proxylist",
+        "url": "https://raw.githubusercontent.com/almroot/proxylist/master/list.txt",
+        "method": "GET",
+        "parser": {
+            "txt": {},
+        }
+    },
+    {
+        "id": "github.com/ShiftyTR/Proxy-List",
+        "url": "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/http.txt",
+        "method": "GET",
+        "parser": {
+            "txt": {},
+        }
+    },
+    {
+        "id": "github.com/mmpx12/proxy-list",
+        "url": "https://raw.githubusercontent.com/mmpx12/proxy-list/master/http.txt",
+        "method": "GET",
+        "parser": {
+            "txt": {},
+        }
+    },
+]
